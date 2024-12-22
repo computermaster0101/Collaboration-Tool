4be8db7e225f3bebf60b112428457aab38e251e7
computermaster0101 <computermaster0101@users.noreply.github.com>
2024-12-22 08:36:16 -0800

    sync changes to socket-server

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 # Replace environment variables in Nginx config
 envsubst '${ROOM_DOMAIN_ADDRESS} ${DOMAIN_ADDRESS}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 envsubst '${SERVICE_URL}' < /opt/excalidraw/excalidraw-app/index.html.template > /opt/excalidraw/excalidraw-app/index.html
+envsubst '${SERVICE_URL}' < /opt/socket-server/static/index.html.template > /opt/socket-server/static/index.html
 
 # Start supervisord
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
